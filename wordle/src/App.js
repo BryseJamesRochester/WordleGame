3c2dfa178d1403fd9becd5b541ca47fc2f7eb2ae
Brandon Ong <brandonong@Brandons-MacBook-Air.local>
2022-02-28 18:31:46 -0800

    Keyboard buttons

---
+++ wordle/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Keyboard from './components/Keyboard.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div>
+   <p>test</p>
+   <Keyboard />
+   </div>
+  );
+}
+
+export default App;
