bf567c0b3ebd38056303937566076c52e17f063e
Aaron Xiang <aaron.xiang@gmail.com>
2022-03-14 15:21:37 -0700

    created user login

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -1,9 +1,28 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Keyboard from './components/Keyboard.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from '../Login/Login';
+import useToken from './useToken';
+
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
 
 function App() {
+  const token = getToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div>
    <p>test</p>
