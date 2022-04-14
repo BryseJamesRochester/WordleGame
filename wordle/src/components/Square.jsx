13a5d5c843720b825052c316e6f206c366bfc336
Brandon Ong <brandonong@Brandons-MacBook-Air.local>
2022-04-13 20:03:02 -0700

    updated square coloring

--- wordle/src/components/Square.jsx
+++ wordle/src/components/Square.jsx
@@ -1,102 +1,92 @@
 import React from "react"
-import { wordAnswer, boardDefault } from "./Words"
+import { wordAnswer, boardDefault, guesses } from "./Words"
 
 function Square(props) {
-  // if (boardDefault[props.col][props.row]) {
-  //   return (
-  //     <svg width="110" height="110" viewBox="0 0 110 110">
-  //     <g>
-  //       <path
-  //         id="imagebot_2"
-  //         fill="none"
-  //         stroke-width="2"
-  //         stroke-miterlimit="10"
-  //         stroke-linecap="square"
-  //         stroke="#000000"
-  //         d="M 5 5 L 5 105 L 105 105 L 105 5 L 5 5 z"
-  //       />
-  //       <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-family="Verdana" font-size="35" fill="black"></text>
-  //     </g>
-  //   </svg>
-  //   )
-  // }
-  // else {
-  //   if(props.guess=="0")
-  //   {
-  //       return (
-  //         <svg width="110" height="110" viewBox="0 0 110 110">
-  //         <g>
-  //           <path
-  //             id="imagebot_2"
-  //             fill="grey"
-  //             stroke-width="2"
-  //             stroke-miterlimit="10"
-  //             stroke-linecap="square"
-  //             stroke="#000000"
-  //             d="M 5 5 L 5 105 L 105 105 L 105 5 L 5 5 z"
-  //           />
-  //           <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-family="Verdana" font-size="35" fill="black">{props.letter}</text>
-  //         </g>
-  //       </svg>
-  //         )
-  //   } else
-  //   if(props.guess=="1")
-  //   {
-  //       return (
-  //         <svg width="110" height="110" viewBox="0 0 110 110">
-  //         <g>
-  //           <path
-  //             id="imagebot_2"
-  //             fill="yellow"
-  //             stroke-width="2"
-  //             stroke-miterlimit="10"
-  //             stroke-linecap="square"
-  //             stroke="#000000"
-  //             d="M 5 5 L 5 105 L 105 105 L 105 5 L 5 5 z"
-  //           />
-  //           <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-family="Verdana" font-size="35" fill="black">{props.letter}</text>
-  //         </g>
-  //       </svg>
-  //         )
-  //   } else
-  //   if(props.guess=="2")
-  //   {
-  //       return (
-  //         <svg width="110" height="110" viewBox="0 0 110 110">
-  //         <g>
-  //           <path
-  //             id="imagebot_2"
-  //             fill="green"
-  //             stroke-width="2"
-  //             stroke-miterlimit="10"
-  //             stroke-linecap="square"
-  //             stroke="#000000"
-  //             d="M 5 5 L 5 105 L 105 105 L 105 5 L 5 5 z"
-  //           />
-  //           <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-family="Verdana" font-size="35" fill="black">{props.letter}</text>
-  //         </g>
-  //       </svg>
-  //     )
-  //   } else //guess has not been checked yet
-  //   return (
-  //     <svg width="110" height="110" viewBox="0 0 110 110">
-  //     <g>
-  //       <path
-  //         id="imagebot_2"
-  //         fill="none"
-  //         stroke-width="2"
-  //         stroke-miterlimit="10"
-  //         stroke-linecap="square"
-  //         stroke="#000000"
-  //         d="M 5 5 L 5 105 L 105 105 L 105 5 L 5 5 z"
-  //       />
-  //       <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-family="Verdana" font-size="35" fill="black">{props.letter}</text>
-  //     </g>
-  //   </svg>
-  //     )
-  // }
-  return (
-        <svg width="110" height="110" viewBox="0 0 110 110">
+  if (guesses[props.col][props.row] == "0") {
+    return (
+      <svg width="110" height="110" viewBox="0 0 110 110">
+        <g>
+          <path
+            id="imagebot_2"
+            fill="grey"
+            stroke-width="2"
+            stroke-miterlimit="10"
+            stroke-linecap="square"
+            stroke="#000000"
+            d="M 5 5 L 5 105 L 105 105 L 105 5 L 5 5 z"
+          />
+          <text
+            x="50%"
+            y="50%"
+            text-anchor="middle"
+            dominant-baseline="middle"
+            font-family="Verdana"
+            font-size="35"
+            fill="black"
+          >
+            {boardDefault[props.col][props.row]}
+          </text>
+        </g>
+      </svg>
+    )
+  } else if (guesses[props.col][props.row] == "1") {
+    return (
+      <svg width="110" height="110" viewBox="0 0 110 110">
+        <g>
+          <path
+            id="imagebot_2"
+            fill="yellow"
+            stroke-width="2"
+            stroke-miterlimit="10"
+            stroke-linecap="square"
+            stroke="#000000"
+            d="M 5 5 L 5 105 L 105 105 L 105 5 L 5 5 z"
+          />
+          <text
+            x="50%"
+            y="50%"
+            text-anchor="middle"
+            dominant-baseline="middle"
+            font-family="Verdana"
+            font-size="35"
+            fill="black"
+          >
+            {boardDefault[props.col][props.row]}
+          </text>
+        </g>
+      </svg>
+    )
+  } else if (guesses[props.col][props.row] == "2") {
+    return (
+      <svg width="110" height="110" viewBox="0 0 110 110">
+        <g>
+          <path
+            id="imagebot_2"
+            fill="green"
+            stroke-width="2"
+            stroke-miterlimit="10"
+            stroke-linecap="square"
+            stroke="#000000"
+            d="M 5 5 L 5 105 L 105 105 L 105 5 L 5 5 z"
+          />
+          <text
+            x="50%"
+            y="50%"
+            text-anchor="middle"
+            dominant-baseline="middle"
+            font-family="Verdana"
+            font-size="35"
+            fill="black"
+          >
+            {boardDefault[props.col][props.row]}
+          </text>
+        </g>
+      </svg>
+    )
+  } //guess has not been checked yet
+  else {
+    return (
+      <svg width="110" height="110" viewBox="0 0 110 110">
         <g>
           <path
             id="imagebot_2"
@@ -107,10 +97,21 @@ function Square(props) {
             stroke="#000000"
             d="M 5 5 L 5 105 L 105 105 L 105 5 L 5 5 z"
           />
-          <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-family="Verdana" font-size="35" fill="black">{boardDefault[props.col][props.row]}</text>
+          <text
+            x="50%"
+            y="50%"
+            text-anchor="middle"
+            dominant-baseline="middle"
+            font-family="Verdana"
+            font-size="35"
+            fill="black"
+          >
+            {boardDefault[props.col][props.row]}
+          </text>
         </g>
       </svg>
-        )
+    )
+  }
 }
 
 export default Square
