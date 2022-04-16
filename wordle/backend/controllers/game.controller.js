59b34788f622a176eb3b56ede83433f1bacba0f0
BryseRochester <bryse.rochester@hotmail.com>
2022-04-16 04:31:28 -0700

    Updated documentation

--- wordle/backend/controllers/game.controller.js
+++ wordle/backend/controllers/game.controller.js
@@ -5,7 +5,9 @@ let GameService = require('../services/game.service');
 
 /**
  * Used to start a new wordle game. 
- * Request body can set numGuesses and wordLength, both are 5 by default.
+ * Retrieves wordlist from database and randomly selects a word, storing initial gamestate in Users "gamestate" field in db
+ * Request body can set numGuesses, default is 5
+ * Request body can also set the username of the player, default is 'guest'
  * Request body can also contain a difficulty parameter which will select words based on assigned difficulty
  * Returns a message indicating game was started successfully, or an error message if an error occurred
  */
@@ -15,8 +17,11 @@ const startGame = async function(req, res, next) {
     let username = req.body.username ? req.body.username : 'guest';
     try {
         let wordList = await WordListService.getDefaultWordList(difficulty);
+
         let selectedWord = Math.floor(Math.random() * wordList.length);
         let secretWord = wordList[selectedWord];
+
+        //gamestate of a new game
         let gamestate = {secretWord:secretWord, remainingGuesses:numGuesses, result:"In Progress", pastGuesses:[]};
         if (UserService.updateUserGameState(username, gamestate))
             return res.status(200).json({ status: 200, message: `Game Started. Word is ${secretWord}`});
@@ -27,7 +32,7 @@ const startGame = async function(req, res, next) {
 
 /**
  * Used to make a guess in an existing wordle game. Game must be started before this can be called.
- * Request body must contain a guess 
+ * Request body must contain a guess and a username
  */
 const makeGuess = async function(req, res, next) {
     const guess = req.body.guess;
