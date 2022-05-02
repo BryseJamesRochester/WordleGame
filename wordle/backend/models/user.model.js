d5f57e53247ad9ed5ef56cb9b621105e46d02af0
BryseRochester <bryse.rochester@hotmail.com>
2022-05-02 16:43:19 -0700

    Small change to clarify db usage

--- wordle/backend/models/user.model.js
+++ wordle/backend/models/user.model.js
@@ -18,36 +18,37 @@ const userSchema = new Schema({
         required: true
     },
     email: {
-        type:String,
-        required:true,
-        unique:true,
-        trim:true,
-        lowercase:true,
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     stats: {
-        gamesPlayed: {type:Number, default:0},
-        gamesWon: {type:Number, default:0},
-        multiplayerGamesPlayed: {type:Number, default:0},
-        multiplayerGamesWon: {type:Number, default:0}
+        gamesPlayed: { type: Number, default: 0 },
+        gamesWon: { type: Number, default: 0 },
+        multiplayerGamesPlayed: { type: Number, default: 0 },
+        multiplayerGamesWon: { type: Number, default: 0 }
     },
-    gamestate: { 
-        secretWord: String, 
-        remainingGuesses: Number, 
-        result: String, 
-        pastGuesses: [String] 
+    gamestate: {
+        secretWord: String,
+        remainingGuesses: Number,
+        result: String,
+        pastGuesses: [String]
     },
     wordlists: [{
-        name:String,
+        name: String,
         words: [String],
         wordLength: Number,
         enabled: Boolean
     },
-        { _id: false }
+    { _id: false }
     ]
-},
-    {timestamps: true,}
-);
+}, {
+    timestamps: true,
+    collection: 'users'
+});
 
 const User = mongoose.model('User', userSchema);
 module.exports = { User };
